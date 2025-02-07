95ca2a3ce43cec18b7d2112784d8bf7593e08e0e
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-07 08:35:07 +0100

    replace: node kind not the same

--- src/lib/__tests__/diff.replace.test.ts
+++ src/lib/__tests__/diff.replace.test.ts
@@ -1,5 +1,5 @@
 import { diffNodes } from '../diff'
-import { VNode } from '../types'
+import { Props, VNode } from '../types'
 
 describe('diffNodes - Replace cases', () => {
   test('returns REPLACE patch when static elements have different types', () => {
@@ -28,7 +28,39 @@ describe('diffNodes - Replace cases', () => {
     ])
   })
 
-  // test('returns REPLACE when static element becomes regular component')
+  test('returns REPLACE when static element becomes regular component', () => {
+    const oldNode: VNode = {
+      kind: 'static',
+      type: 'div',
+      props: {},
+      children: [],
+    }
+
+    const ComponentFn = (props: Props): VNode => ({
+      kind: 'static',
+      type: 'div',
+      props,
+      children: [],
+    })
+
+    const newNode: VNode = {
+      kind: 'regular',
+      type: ComponentFn,
+      props: {},
+      children: [],
+    }
+
+    const patches = diffNodes(oldNode, newNode)
+
+    expect(patches).toEqual([
+      {
+        type: 'REPLACE',
+        oldNode,
+        newNode,
+      },
+    ])
+  })
+
   // test('returns REPLACE when regular component becomes static element')
   // test('returns REPLACE when regular component becomes memo component')
   // test('returns REPLACE when memo component becomes regular component')
--- src/lib/diff.ts
+++ src/lib/diff.ts
@@ -1,8 +1,15 @@
 import { Patch, VNode } from './types'
 
-// The diffing function
 export function diffNodes(oldNode: VNode, newNode: VNode): Array<Patch> {
   const patches: Array<Patch> = []
+
+  if (oldNode.kind !== newNode.kind) {
+    patches.push({
+      type: 'REPLACE',
+      oldNode,
+      newNode,
+    })
+  }
 
   if (oldNode.kind === 'static' && newNode.kind === 'static') {
     if (oldNode.type !== newNode.type) {
